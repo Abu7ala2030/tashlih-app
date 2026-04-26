ed32bf40c20ac58709ef5a3a74adffd26737fb97
Dhaafi Abdu <Dhaafi.abdu@hotmail.com>
2026-04-26 22:51:32 +0300

    Remove duplicate customer bottom navigation

--- lib/shared/layout/customer_shell.dart
+++ lib/shared/layout/customer_shell.dart
@@ -1,43 +1,12 @@
 import 'package:flutter/material.dart';
 
 import '../../features/customer/home/customer_home_screen.dart';
-import '../../features/customer/profile/customer_profile_screen.dart';
-import '../../features/customer/requests/my_requests_screen.dart';
-import '../../features/customer/search/search_screen.dart';
 
-class CustomerShell extends StatefulWidget {
+class CustomerShell extends StatelessWidget {
   const CustomerShell({super.key});
 
   @override
-  State<CustomerShell> createState() => _CustomerShellState();
-}
-
-class _CustomerShellState extends State<CustomerShell> {
-  int currentIndex = 0;
-
-  final pages = const [
-    CustomerHomeScreen(),
-    SearchScreen(),
-    MyRequestsScreen(),
-    CustomerProfileScreen(),
-  ];
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[currentIndex],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: currentIndex,
-        onDestinationSelected: (index) {
-          setState(() => currentIndex = index);
-        },
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.home_outlined), label: 'الرئيسية'),
-          NavigationDestination(icon: Icon(Icons.search), label: 'البحث'),
-          NavigationDestination(icon: Icon(Icons.list_alt_outlined), label: 'طلباتي'),
-          NavigationDestination(icon: Icon(Icons.person_outline), label: 'حسابي'),
-        ],
-      ),
-    );
+    return const CustomerHomeScreen();
   }
 }
